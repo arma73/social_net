6fcec9e323c72abaada2563465da9bf1264a8a1b
Arman Khilko <arma.biz9805@gmail.com>
2020-12-21 15:18:53 +0400

    Add: main page in route /

---
+++ src/pages/public/main/Main.jsx
@@ -0,0 +1,34 @@
+import { useHistory } from "react-router-dom";
+import Box from "_components/box";
+import Layout from "_components/layout";
+import Img from "_components/img";
+import Button from "_components/form/button";
+
+import photoBgImage from "_assets/images/bg/axel_bg.jpg";
+import { layout, box, button, bgImage } from "./style.m.css";
+
+const Main = () => {
+    const history = useHistory();
+    const handleRoute = () => history.push("/gallery");
+
+    return (
+        <Layout view={{ "center": true }} className={layout}>
+            <Box view={{ "size": "full", "center": true }} className={box}>
+                <Img
+                    src={photoBgImage}
+                    component="div"
+                    className={bgImage}
+                />
+                <Button
+                    style={{ "position": "absolute" }}
+                    onClick={handleRoute}
+                    className={button}
+                >
+                    Gallery
+                </Button>
+            </Box>
+        </Layout>
+    );
+};
+
+export default Main;
