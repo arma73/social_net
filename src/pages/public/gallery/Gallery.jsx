09e546f6f6aee82c5dde322e171bebde38251977
Arman Khilko <arma.biz9805@gmail.com>
2020-12-21 17:54:15 +0400

    Add: gallery page

---
+++ src/pages/public/gallery/Gallery.jsx
@@ -0,0 +1,10 @@
+import Layout from "_components/layout";
+import { layout } from "./style.m.css";
+
+const Gallery = () => (
+    <Layout className={layout}>
+        Gallery
+    </Layout>
+);
+
+export default Gallery;
