0bba88d2ec20845a4e7bba594bfdf14375677198
Arman Khilko <arma.biz9805@gmail.com>
2020-12-21 17:57:37 +0400

    Update: composition of components gallery

--- src/pages/public/gallery/Gallery.jsx
+++ src/pages/public/gallery/Gallery.jsx
@@ -1,6 +1,22 @@
 import Layout from "_components/layout";
 import { layout } from "./style.m.css";
 
+// NOTE: components ➡ ProfileHead [
+//    ProfileAvatar, 
+//    ProfileData[
+//        ProfileName, 
+//        ProfilePresent[Followers], 
+//        ProfileDescription
+//    ]
+// ]
+
+// NOTE: components ➡ Posts [
+//    PostsHead,    
+//    Grid[
+//      FlexRow[FlexItem] ?? GridRow[GridItem]
+//    ]
+// ]
+
 const Gallery = () => (
     <Layout className={layout}>
         Gallery
