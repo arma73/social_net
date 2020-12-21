6dfc30565e1f076767d13664804495905d650a1a
Arman Khilko <arma.biz9805@gmail.com>
2020-12-21 18:04:43 +0400

    Rework: optional chain to view.center

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -2,7 +2,7 @@ import clsx from "clsx";
 import { layout, center } from "./style.m.css";
 
 const Layout = ({ children, view, className, ...restProps }) => (
-    <main className={clsx(layout, className, { [center]: view.center })} {...restProps}>
+    <main className={clsx(layout, className, { [center]: view?.center })} {...restProps}>
         {children}
     </main>
 );
