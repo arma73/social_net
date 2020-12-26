195a9c3c3a5b276b19400f6956165d9a1b36785f
Arman Khilko <arma.biz9805@gmail.com>
2020-12-26 12:08:44 +0400

    Create: component row/MixedItems

---
+++ src/components/row/mixed/items/MixedItems.jsx
@@ -0,0 +1,27 @@
+import Img from "_components/img";
+import random from "_utils/random";
+
+import { item, itemWide, itemTall, itemBig, img } from "./style.m.css";
+
+const MixedItems = ({ className, content }) => {
+    const mapItems = () => {
+        const sizes = ["", itemWide, itemTall, itemBig];
+        return content.map(photo => {
+            const view = random(0, 3, sizes);
+            return (
+                <div className={`${item} ${view} ${className}`} key={photo.id}>
+                    <Img
+                        src={`public/assets/images/gallery/${photo.fileName}`}
+                        component="img"
+                        className={img}
+                        style={{ "objectFit": "cover" }}
+                    />
+                </div>
+            );
+        });
+    };
+    
+    return mapItems();
+};
+
+export default MixedItems;
