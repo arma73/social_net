0ed90b071cf80230864d3543c11adbd5eb7216e1
Arman Khilko <arma.biz9805@gmail.com>
2020-12-26 12:06:38 +0400

    Create: component row/GridItems

---
+++ src/components/row/grid/items/GridItems.jsx
@@ -0,0 +1,26 @@
+import Img from "_components/img";
+import random from "_utils/random";
+
+import { item, itemMedium, itemLarge, itemFull } from "./style.m.css";
+
+const GridItems = ({ className, content }) => {
+    const mapItems = () => {
+        const sizes = ["", itemMedium, itemLarge, itemFull];
+        return content.map(photo => {
+            const view = random(0, 3, sizes);
+            return (
+                <div className={`${item} ${view} ${className}`} key={photo.id}>
+                    <Img
+                        src={`public/assets/images/gallery/${photo.fileName}`}
+                        component="img"
+                        style={{ "objectFit": "cover" }}
+                    />
+                </div>
+            );
+        });
+    };
+
+    return mapItems();
+};
+
+export default GridItems;
