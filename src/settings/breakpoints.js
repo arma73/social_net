c907bc9f028e21b1d234bac49a526c8038cd90f6
Arman Khilko <arma.biz9805@gmail.com>
2020-12-26 12:32:47 +0400

    Add: breakpoints

---
+++ src/settings/breakpoints.js
@@ -0,0 +1,11 @@
+export const breakpoints = {
+    "desktopLg": 1400,
+    "desktopMd": 1300,
+    "desktopSm": 1200,
+    "tabletLg": 1040,
+    "tabletMd": 991,
+    "tabletSm": 840,
+    "mobileLg": 767,
+    "mobileMd": 540,
+    "mobileSm": 400
+};
