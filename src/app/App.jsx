58104510cade207c8dfaebe79d6220b3cbb4f5f7
Arman Khilko <arma.biz9805@gmail.com>
2020-12-26 12:31:48 +0400

    Rework: inclusion of redux to the project

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,15 +1,19 @@
 import { hot } from "react-hot-loader/root";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 import Routes from "_routes/Routes";
+import store from "_store";
 
 import "normalize.css";
 import "_theme/main.css";
 
 const App = () => (
     <React.StrictMode>
-        <BrowserRouter>
-            <Routes />
-        </BrowserRouter>
+        <Provider store={store}>
+            <BrowserRouter>
+                <Routes />
+            </BrowserRouter>
+        </Provider>
     </React.StrictMode>
 );
 
