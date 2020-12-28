199963d4d8adbf3fca7fed8152f4f7bf8792b923
Arman Khilko <arma.biz9805@gmail.com>
2020-12-28 13:54:46 +0400

    Create: constants for routes

---
+++ src/settings/routes.js
@@ -0,0 +1,5 @@
+export const MAIN_PAGE_ROUTE = "/";
+export const SELF_PAGE_ROUTE = "/self";
+export const PICK_PHOTO_ROUTE = "/p/";
+export const PICK_PHOTO_RELATIVE_ROUTE = `${PICK_PHOTO_ROUTE}:id`;
+export const PICK_PHOTO_PAGE_RELATIVE_ROUTE = `${PICK_PHOTO_ROUTE}:id`;
